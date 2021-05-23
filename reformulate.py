821de3a44aa4f67968b257fab0ae2d80473cbe25
Ralfs Sedlenieks <rufle23@gmail.com>
2021-05-23 21:47:30 +0300

    Added comments

--- reformulate.py
+++ reformulate.py
@@ -19,14 +19,17 @@ if __name__ == '__main__':
     print('\n ### Reading file: {} \n'.format(infile))
 
     with open(infile, 'r', encoding='utf_8') as f:
+        # Nolasam faila esoso vaicajumu
         text_in = f.read()
 
         print('\n ### SPARQL query from file: \n')
         print(text_in)
 
+        # Attiram vaicajuma jaunas rindas simbolus
         text_in = re.sub(r'\r\n', ' ', text_in)
         text_in = re.sub(r'\n', ' ', text_in)
 
+        # Sagatavojam un izpildam pieprasijumu Ontop tulkosanas galapunktam
         payload = {"query": text_in}
         response = requests.get(
             "http://192.168.99.100:8080/ontop/reformulate", params=payload
